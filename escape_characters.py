983447ebb2bfad5147fd80ee7ae7fceb728c2a93
Isa Bell <160649689+ScriptingSiren13@users.noreply.github.com>
2024-03-07 23:42:29 +0530

    Add files via upload

---
+++ escape_characters.py
@@ -0,0 +1,38 @@
+#1. Single Quote(\'s):
+# txt='It\'s alright.'
+# print(txt)  #It's alright.
+
+#2. Backslash(\\):
+# txt="This will insert one \\ (backslash.)"
+# print(txt) #This will insert one \ (backslash.)
+
+#3. New line (\n):
+# txt="Welcome\n Home"
+# print(txt)  Welcome
+#             Home
+
+#4. Carriage return(\r):
+# print("Hello\rWorld")
+
+#5. Tab(\t):
+# print("Name:\tIsa")
+# print("Age:\t22")
+# print("City:\tMilan")  
+#OUTPUT: 
+# Name:   Isa
+# Age:    22
+# City:   Milan
+
+#6. Backspace(\b)
+# txt="Hello \bWorld!"
+# print(txt)   #HelloWorld!
+
+# text="Hello\bWorld!"
+# print(text)  #HellWorld!
+
+# #7. Octal (\ooo):
+# txt = "\110\145\154\154\157"
+# print(txt)   #Hello
+
+#8. Hex value(\xhh):
+print('\x76\x6C') #vl
